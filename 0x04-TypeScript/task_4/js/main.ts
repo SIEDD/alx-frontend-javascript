2765e880696e3e774c2bf98bdd2f1bb7916fd744
siedd <saidmwacharo8@gmail.com>
2025-01-15 21:10:11 +0300

    error

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,1 +1,33 @@
+import { Subjects } from './subjects/Cpp';
+import { Subjects as JavaSubjects } from './subjects/Java';
+import { Subjects as ReactSubjects } from './subjects/React';
 
+// Export constants
+export const cpp = new Subjects.Cpp();
+export const java = new JavaSubjects.Java();
+export const react = new ReactSubjects.React();
+
+// Create a teacher object
+export const cTeacher: Subjects.Teacher = {
+    firstName: 'John',
+    lastName: 'Doe',
+    experienceTeachingC: 10,
+};
+
+// Cpp subject
+console.log('C++');
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+// Java subject
+console.log('Java');
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+// React subject
+console.log('React');
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
