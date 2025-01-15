05b732f86b2cf8fd4e2d2b7064e952417c9ef870
siedd <saidmwacharo8@gmail.com>
2025-01-15 16:20:36 +0300

    Outputs

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,6 +7,10 @@ interface Teacher {
     [key: string]: any;
   }
   
+  interface Directors extends Teacher {
+    numberOfReports: number;
+  }
+  
   const teacher3: Teacher = {
     firstName: 'Said',
     fullTimeEmployee: false,
@@ -17,3 +21,49 @@ interface Teacher {
   
   console.log(teacher3);
 
+  const director1: Directors = {
+    firstName: 'Said',
+    lastName: 'Baidoo',
+    location: 'Mombasa',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+  };
+  
+  console.log(director1);
+  
+  interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+  }
+  
+  const printTeacher: printTeacherFunction = (firstName, lastName) => {
+    return `${firstName.charAt(0)}. ${lastName}`;
+  };
+  
+  console.log(printTeacher("Said", "Baidoo"));
+  
+  interface StudentClassConstructor {
+    new (firstName: string, lastName: string): StudentClassInterface;
+  }
+  
+  interface StudentClassInterface {
+    workOnHomework(): string;
+    displayName(): string;
+  }
+  
+  class StudentClass implements StudentClassInterface {
+    constructor(public firstName: string, public lastName: string) {}
+  
+    workOnHomework(): string {
+      return "Currently working";
+    }
+  
+    displayName(): string {
+      return this.firstName;
+    }
+  }
+  
+  const student1 = new StudentClass("Modi", "Irah");
+  console.log(student1.displayName());
+  console.log(student1.workOnHomework());
+
+  
