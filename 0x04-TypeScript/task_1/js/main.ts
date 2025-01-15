04ecfbdbd95cd019f41645f85badffd6ec6b4f2b
siedd <saidmwacharo8@gmail.com>
2025-01-15 16:09:18 +0300

    late

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,1 +1,19 @@
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any;
+  }
+  
+  const teacher3: Teacher = {
+    firstName: 'Said',
+    fullTimeEmployee: false,
+    lastName: 'Baidoo',
+    location: 'Mombasa',
+    contract: false,
+  };
+  
+  console.log(teacher3);
 
